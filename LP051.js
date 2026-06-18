f9d7f1e526d5010b6862ac73ae0cca1b43f2a998
Maria Eduarda Lima <marianit20152003@gmail.com>
2026-06-18 10:15:13 -0300

    exercicios de repeticao

---
+++ LP051.js
@@ -0,0 +1,152 @@
+// exercício 1
+
+
+// com while do
+
+// let contador = 1;
+// do {
+//     console.log(`${contador}`);
+//     contador ++;    
+// } while (contador < 101)
+
+// com while
+
+// let contador = 1;
+
+// while ( contador <= 100){
+//     console.log(contador);
+//     contador ++;
+// }
+
+
+// usando for
+
+// for (let i = 1; i < 101; i++) {
+//     console.log(i)
+// }
+
+// exercício 2
+
+// usando for
+
+// for (let i = 100; i > 0; i--){
+//     console.log(i)
+// }
+
+// usando while do
+
+// let contador = 100;
+// do{
+//     console.log(contador);
+//     contador --;   
+// }while (contador > 0)
+
+//usando while 
+
+// let contador = 100;
+// while (contador > 0){
+//     console.log(contador);
+//     contador --;
+// }
+
+
+// exercicio 3
+
+// usando while do
+
+// let num = 15;
+// do{
+//     let quad = num ** 2;
+//     console.log(quad)
+//     num ++;
+// }while(num < 200);
+
+// usando while
+
+// let num = 15;
+// while(num < 200){
+//     let quad = num ** 2;
+//     console.log(quad);
+//     num ++;
+// }
+
+// usando for
+
+// for(let i = 15; i <= 200; i++){
+//     console.log(i**2);
+// }
+
+// exercicio 4
+
+//usando while
+
+// let num = 1;
+// let soma = 0;
+// while(num <= 100){
+//     soma = soma +  num ;
+//     num ++;
+// }
+// console.log(soma);
+
+//usando for
+
+// let soma = 0;
+// for(let i = 1;i<=100; i++){
+//     soma = soma + i;
+// }
+// console.log(soma);
+
+//usando while do
+
+// let soma = 0;
+// let num = 1;
+// do{
+//     soma = soma + num;
+//     num ++;
+// }while(num <= 100)
+// console.log(soma)
+
+//exercicio 5
+
+//usando while
+// let contador = 1
+// let numero = 3
+// while(contador <= 10){
+//     let resul = numero * contador
+//     console.log(`${numero}.${contador} = ${resul}`)
+//     contador ++
+// }
+
+//usando for 
+// let numero = 2
+// for(let i = 1; i<= 10; i++){
+//     let resul = numero * i
+//     console.log(`${numero}.${i} = ${resul}`)
+// }
+
+//usando do while 
+
+// let numero = 6
+// contador = 1
+// do{
+//     let resul = numero * contador
+//     console.log(`${numero}.${contador} = ${resul}`)
+//     contador ++
+// }while(contador<=10)
+
+//exercicio 6
+
+//usando while
+// let n = 60;
+// let produto = 0;
+
+// if(n >= 50){
+//     console.log(`numero invalido`)
+// }else{
+//     while(produto < 250){
+//     console.log(produto)
+//     produto  += (n * 3)
+// }
+// }
+
+
