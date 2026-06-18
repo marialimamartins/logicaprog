16a9a848f0cf353ddfa9b078cd64941b57227261
Maria Eduarda Lima <marianit20152003@gmail.com>
2026-06-18 08:30:19 -0300

    lista 6

---
+++ LP0611.js
@@ -0,0 +1,32 @@
+let resposta = 3;
+
+console.log("Menu de opções:");
+console.log("1: Incluir");
+console.log("2: Alterar");
+console.log("3: Excluir");
+console.log("4: Pesquisar");
+console.log("5: Sair");
+
+
+switch (resposta){
+    case 1:
+        console.log(`incluindo`)
+        break;
+    case 2:
+        console.log(`alterando`)
+        break;
+    case 3:
+        console.log(`excluindo`)
+        break;
+    case 4:
+        console.log(`pesquisando`)
+        break;
+    case 5:
+        console.log(`saindo`)
+    default:
+        console.log(`resposta inválida para resposta`)
+}
+
+
+
+
---
+++ LP0612.js
@@ -0,0 +1,12 @@
+sexo = "feminino"
+
+switch(sexo){
+    case "feminino":
+        console.log(`banheiro a esquerda`)
+        break;
+    case "masculino":
+        console.log(`banheiro a direita`)
+        break;
+    default:
+        console.log(`não temos banheiro para este sexo`)
+}
